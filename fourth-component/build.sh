1df385eca2082404a09d3a17f707ee74ed29fa98
Maximilian Schmidt <maximilian5189@gmail.com>
2021-02-22 19:42:41 +0100

    consider production build

--- fourth-component/build.sh
+++ fourth-component/build.sh
@@ -3,5 +3,10 @@ cd react-app
 npm install
 yarn build
 searchstring=/static
-replacestring=http://localhost:9004/static
+if [ $1 == 'production' ] 
+then
+  replacestring=http://my-backend.ddns.net:9004/static
+else
+  replacestring=http://localhost:9004/static
+fi
 sed -i.bak "s+${searchstring}+${replacestring}+g" build/index.html && rm build/index.html.bak
